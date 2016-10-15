d196972312c7a822f32ae4e4564fca1d837edd66
Joni Weiss <joni@handmadepixels.com>
2016-10-15 10:25:42 -0600

    refactor(core) Removed console.logs.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
-console.log('Hi!');
+console.log('Begin', document.title);
 
 function displayOutput(orgListOutput) {
   document.querySelector('#output').innerHTML = orgListOutput;
 }
 
 function buildOrgOutput(orglist) {
-  console.log("gitHubOrgs Obj: ", orglist);
   let outputMsg = '';
   for (var i = 0; i < orglist.length; i++) {
     for (el in orglist[i]) {
       if (el === 'login') {
-        console.log("GutHub Organization: " + orglist[i][el]);
         outputMsg += "<h3>" + orglist[i][el] + "</h3><br />";
       }
       if (el === 'url') {
-        console.log("Github API URL: " + orglist[i][el]);
         outputMsg += "<p><a href=\'" + orglist[i][el] + "\' target='_blank'>" + orglist[i][el] + "</a></p><br /><br />";
       }
     }
@@ -25,7 +22,6 @@ function buildOrgOutput(orglist) {
 function getGithubOrgs() {
   let gitHubOrgs = $.get('https://api.github.com/users/jisaacks/orgs')
       .done(function(data) {
-        console.log("gitHubOrgs Obj: ", data);
         buildOrgOutput(data);
       });
 }
